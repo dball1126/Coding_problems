fa5993e92594d35155796548069c2fab6643c90b
daniel <g.thirteen333@gmail.com>
2019-11-07 22:14:12 -0500

    distributeCandies function complete

--- distributeCandies.js
+++ distributeCandies.js
@@ -1,12 +1,23 @@
 var distributeCandies = function (candies, num_people) {
-    let max = 2 * candies, people = [], i = 0;
-   
-    while (i < num_people) { 
-        people.push(0) 
-        i++;
+    let max = 2 * candies, people = [], i = 0, count = 1;;
+
+    people = new Array(num_people).fill(0)
+    let iter = 0;
+    while (candies > 0 || people[people.length-1] === max) {
+
+            if (count > candies) {
+                people[iter] += candies;
+            } else {
+                people[iter] += count;
+            }
+
+            candies -= count;
+            count +=1;
+            iter += 1
+            if (iter >= people.length) iter = 0;
     }
-    console.log(people)
+    return people;
 };
 
 
-console.log(distributeCandies(7, 4))
+console.log(distributeCandies(10, 3))
