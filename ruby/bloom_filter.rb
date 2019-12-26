b562a0cae178f181c3a68e8130ac4108f60a87a9
daniel <g.thirteen333@gmail.com>
2019-12-25 21:21:43 -0500

    Create storage successfully creates the array with the right size

--- ruby/bloom_filter.rb
+++ ruby/bloom_filter.rb
@@ -3,15 +3,23 @@ class BloomFilter
 
     def initialize(size = 62000)
         if size > 62000
-            self.size = 62000
+            @size = 62000
         else
-            self.size = size
+            @size = size
         end
-        self.storage = create_storage
+        @storage = create_storage(@size)
     end
 
     def create_storage(size)
+        internal_storage = []
+
+        for index in 0..size do
+            internal_storage.push(false)
+        end
         
     end
 
 end
+
+ p obj = BloomFilter.new(100)
+p obj
