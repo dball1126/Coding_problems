93e23513c8119651a44c5e6746f0504231291548
daniel <g.thirteen333@gmail.com>
2019-12-25 22:41:25 -0500

    all hash functions created, tested, and working

--- ruby/bloom_filter.rb
+++ ruby/bloom_filter.rb
@@ -7,19 +7,62 @@ class BloomFilter
         else
             @size = size
         end
-        @storage = create_storage(@size)
-    end
-
-    def create_storage(size)
-        internal_storage = []
-
+        @storage = []
         for index in 0..size do
-            internal_storage.push(false)
+            @storage.push(false)
         end
-        
     end
 
+    def get_value(index)
+        @storage[index]
+    end
+
+    def set_value(index)
+        @storage[index] = true
+    end
+
+    def insert(item)
+        hashed_values = self.get_hashed_values(item)
+    end
+
+    def get_hashed_values(item)
+        [self.hash1(item), self.hash2(item), self.hash3(item)]
+    end
+
+    def hash1(item)
+        hash1 = 0
+
+        item.split("").each do |char|
+            hash1 = (hash1 << 3) + char.ord + hash1
+            hash1 = hash1 & 0xFFFF_FFFF #Convert to 32 bit signed integer
+            hash1 = (hash1).abs
+        end
+        hash1 % self.storage.size
+    end
+
+    def hash2(item)
+        hash2 = 6731
+
+        item.split("").each do |char|
+            hash2 = char.ord + hash2
+            hash2 = hash2 & 0xFFFF_FFFF #Convert to 32 bit signed integer
+            hash2 = (hash2).abs
+        end
+        hash2 % self.storage.size
+    end
+
+    def hash3(item)
+        hash3 = 0
+
+        item.split("").each do |char|
+            hash3 = (hash3 << 5) + char.ord - hash3
+            hash3 = hash3 & 0xFFFF_FFFF #Convert to 32 bit signed integer
+            hash3 = (hash3).abs
+        end
+        hash3 % self.storage.size
+    end
 end
 
  p obj = BloomFilter.new(100)
-p obj
+p obj.set_value(0)
+p obj.hash3("str")
