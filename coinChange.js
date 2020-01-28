fd75d9550c52205b21b007a7d844c46da2ef46ce
Daniel Ball <g.thirteen333@gmail.com>
2020-01-28 13:21:30 -0500

    coinChange function complete in js

--- coinChange.js
+++ coinChange.js
@@ -4,21 +4,19 @@ Write a function to compute the fewest number of coins that you need to make up 
 If that amount of money cannot be made up by any combination of the coins, return -1.
 */
 
-const coinChange =  (coins, amount, memo = {}) => {
-  if (amount in memo) return memo[amount];
-    if (amount <= 0) return 0;
-    let results = [];
+const coinChange =  (coins, amount) => {
+  let dp = new Array(amount + 1).fill(amount + 1);
+      dp[0] = 0;
 
-    for(coin in coins) {
-      
-      if (coin <= amount) {
-        
-        results.push(coinChange(coins, (amount-coin), memo) + 1);
-      }    
-    } 
-    memo[amount] = Math.min(...results);
-    
-    return memo[amount];
+      for (let i = 1; i < dp.length; i++) {
+        for (let c = 0; c < coins.length; c++) {
+          let coin = coins[c];
+          if (coin <= i) {
+            dp[i] = Math.min(dp[i], 1 + dp[i - coin])
+          }
+        }
+      }
+      return dp[amount];
 };
 
 console.log(coinChange(coins = [1, 2, 5], amount = 11))
